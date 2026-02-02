2f9fb82fdfa6e28b91bc6f20d866e7630c5eeb9e
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-02-02 17:34:59 +0100

    [MODEL] Mise à jour des models
    J'ai remis les id + message_id pour l'historique des messages

--- back/model/message.py
+++ back/model/message.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 
 class Message(BaseModel):
+    id: int
+    user_id: int
     prompt: str
     ai_answer: str
     date: datetime
+    ancient_message_id: int
--- back/model/user.py
+++ back/model/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, SecretStr
 
 class User(BaseModel):
+    id: int
     email: EmailStr
     hashed_password: SecretStr
