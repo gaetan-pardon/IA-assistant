1434852cebb81ed36610131dacc870150c0a7b7d
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-02-02 16:56:00 +0100

    [FIX MODELS] retrait champ id des models

--- back/model/message.py
+++ back/model/message.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 
 class Message(BaseModel):
-    id: int
     prompt: str
     ai_answer: str
     date: datetime
--- back/model/user.py
+++ back/model/user.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, EmailStr, SecretStr
 
 class User(BaseModel):
-    id: int
     email: EmailStr
     hashed_password: SecretStr
