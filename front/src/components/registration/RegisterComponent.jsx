2771ac93c15c63e7d692418f796e25285c097e2c
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-02-03 11:53:22 +0100

    [REG_FORM] Formulaire complet mais pas lié au back

--- front/src/components/registration/RegisterComponent.jsx
+++ front/src/components/registration/RegisterComponent.jsx
@@ -9,35 +9,46 @@ export function RegistrationForm() {
 
     async function handleSubmit() {
         const form = document.getElementById('registration-form');
+        if (!form.elements['email']) {
+            setError("Email must not be empty.");
+            return;
+        }
         const email = form.elements['email'].value;
         const password = form.elements['password'].value;
         const confirmedPassword = form.elements['confirmed_password'].value;
 
-        if (password != confirmedPassword)
+        if (password !== confirmedPassword)
         {
             setError("Passwords do not match");
             return;
         }
 
-        const response = await registerUser(email, password);
-        if (response.status !== 201) {
-            setError(response.details || "Registration failed");
-        } else {
-            setError(null);
+        try {
+            const response = await registerUser(email, password);
+            if (response.status !== 201) {
+                setError(response.details || "Registration failed");
+                return;
+            } else {
+                setError(null);
+            }
+        } catch (error) {
+            setError(error.message || "Network error");
         }
     }
 
 
     return (
-        <form className="registration-form" onSubmit={async (e) => { e.preventDefault(); handleSubmit(); }}>
-            <label>Email</label>
-            <input type='email' name="email" placeholder="Enter your email"/>
-            <label>Password</label>
-            <input type='password' name="password" placeholder="Enter your password"/>
-            <label>Confirm your password</label>
-            <input type='password' name="confirmed_password" placeholder="Confirm your password"/>
-            <button type="submit">Create account</button>
+        <section>
+            <form id="registration-form" className="registration-form" onSubmit={async (e) => { e.preventDefault(); handleSubmit(); }}>
+                <label>Email</label>
+                <input type='email' name="email" placeholder="Enter your email" required />
+                <label>Password</label>
+                <input type='password' name="password" placeholder="Enter your password" pattern=".{8,72}" required title="Your password must be at least 8 characters long and at most 72."/>
+                <label>Confirm your password</label>
+                <input type='password' name="confirmed_password" placeholder="Confirm your password" pattern=".{8,72}" required title="Your password must be at least 8 characters long and at most 72." />
+                <button type="submit">Create account</button>
+            </form>
             <div>{error}</div>
-        </form>
+        </section>
     )
 }
