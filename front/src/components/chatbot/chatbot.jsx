dd766086d44ccc63db38b97f664842cc5f86f246
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-02-06 11:18:46 +0100

    Résolution du problème de suppression

--- front/src/components/chatbot/chatbot.jsx
+++ front/src/components/chatbot/chatbot.jsx
@@ -10,7 +10,7 @@ export default function ChatBot() {
         const [ isAuthenticated, setIsAuthenticated ] = useState(null);
         const [ loading, setLoading ] = useState(true);
         const [ histories, setHistories ] = useState(null);
-        const [ currentHistory, setCurrentHistory ] = useState([]);
+        const [ currentHistory, setCurrentHistory ] = useState(null);
         const [ messages, setMessages ] = useState([]);
         const [ inputText, setInputText ] = useState("");
         const [ loadingAIResponse, setLoadingAIResponse ] = useState(false);
@@ -46,6 +46,9 @@ export default function ChatBot() {
         const handleSendMessage = () => {
             if (loadingAIResponse)
                 return;
+            if (!currentHistory) {
+                return;
+            }
             if (inputText.trim()) {
                 setMessages(prev => [...prev, { text: inputText, sender: "user" }]);
                 setInputText("");
@@ -68,6 +71,7 @@ export default function ChatBot() {
 
         const changeHistory = (history) => {
             setCurrentHistory(history);
+            setMessages([]);
             fetchHistoryById(history.id).then((response) => {
                 if (response.status === 200 && response.data?.messages) {
                     if (!Array.isArray(response.data.messages)) {
@@ -106,17 +110,8 @@ export default function ChatBot() {
                         setHistories([]);
                     });
                     if (currentHistory.id === historyId) {
-                       createHistory().then((response) => {
-                            if (response.status === 201) {
-                                setCurrentHistory(response.data);
-                                setMessages([]);
-                                setIsAuthenticated(true);
-                            } else {
-                                setIsAuthenticated(false);
-                            }
-                        }).catch(() => {
-                            setIsAuthenticated(false);
-                        });
+                        setCurrentHistory(null);
+                        setMessages([]);
                     }
                 }
             }).catch((error) => {
@@ -138,36 +133,38 @@ export default function ChatBot() {
                 <h3>Historiques</h3>
                 {histories && histories.map((history) => (
                     <div key={history.id} className="chat-item-container">
-                        <button key={history.id} className="chat-item" onClick={() => changeHistory(history)} disabled={currentHistory.id === history.id || loadingAIResponse}>
+                        <button key={history.id} className="chat-item" onClick={() => changeHistory(history)} disabled={currentHistory?.id === history.id || loadingAIResponse}>
                             <span>{history.name}</span>
                         </button>
                         <button onClick={() => deleteHistory(history.id)}>🗑</button>
                     </div>
                 ))}
             </section>
-            <section className="chat">
-                <div className="messages-container">
-                    {messages.length > 0 ? messages.map((message, index) => (
-                        <div key={index} className={`message ${message.sender}`}>
-                            <ReactMarkdown>{message.text}</ReactMarkdown>
-                        </div>
-                )) : <div className="no-messages">Aucun message pour le moment. Commencez la conversation !</div>}
-                </div>
-                
-                <div className="input-container">
-                    <input
-                        type="text"
-                        className="prompt-input"
-                        placeholder="Écrivez votre message..."
-                        value={inputText}
-                        onChange={(e) => setInputText(e.target.value)}
-                        onKeyUp={handleKeyPress}
-                    />
-                    <button className="send-button" onClick={handleSendMessage} disabled={loadingAIResponse}>
-                        Envoyer
-                    </button>
-                </div>
-            </section>
+            {currentHistory && (
+                <section className="chat">
+                    <div className="messages-container">
+                        {messages.length > 0 ? messages.map((message, index) => (
+                            <div key={index} className={`message ${message.sender}`}>
+                                <ReactMarkdown>{message.text}</ReactMarkdown>
+                            </div>
+                    )) : <div className="no-messages">Aucun message pour le moment. Commencez la conversation !</div>}
+                    </div>
+                    
+                    <div className="input-container">
+                        <input
+                            type="text"
+                            className="prompt-input"
+                            placeholder="Écrivez votre message..."
+                            value={inputText}
+                            onChange={(e) => setInputText(e.target.value)}
+                            onKeyUp={handleKeyPress}
+                        />
+                        <button className="send-button" onClick={handleSendMessage} disabled={loadingAIResponse}>
+                            Envoyer
+                        </button>
+                    </div>
+                </section>
+            )}
         </div>
     )
 }
