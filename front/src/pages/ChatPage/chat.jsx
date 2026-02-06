8b95520b5cb099037d4485dd06eea8c39e6f8510
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-02-06 11:24:23 +0100

    Titre mis au centre

--- front/src/pages/ChatPage/chat.jsx
+++ front/src/pages/ChatPage/chat.jsx
@@ -1,9 +1,10 @@
+import "./chat.css";
 import ChatBot from "../../components/chatbot/chatbot"; 
 
 export default function ChatPage() {
     return (
         <div className="chat-component">
-            <h1>Chat 2GP</h1>
+            <h1 className="chat-title">Chat 2GP</h1>
             <ChatBot />
         </div>
     )
